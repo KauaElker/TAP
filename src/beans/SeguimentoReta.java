0076801c24bfcc3fe6c05b0c0da3d9f97790aa77
KauaElker <162702755+KauaElker@users.noreply.github.com>
2024-03-20 12:00:22 -0300

    Add files via upload

---
+++ src/beans/SeguimentoReta.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author aluno
+ */
+public class SeguimentoReta {
+    private Ponto2D = p0;
+   
+    private Ponto2D = p1;
+
+    public SeguimentoReta() {
+    }
+    
+}
---
+++ src/beans/objeto2D.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author aluno
+ */
+public interface objeto2D {
+    public void moveX(double dX);
+    public void moveY(double dY);
+    public void moveXY(double dX, double dY);
+}
+
